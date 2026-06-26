0215cd1f2c34d2a21ef382248cc1d1ff2828fa76
Carlos Vera <vera.0214@outlook.com>
2026-06-26 17:00:25 -0500

    Initial Commit

---
+++ MusicAlbum/MusicAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAlbumApp.swift
+//  MusicAlbum
+//
+//  Created by Carlos Vera on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
